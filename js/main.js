ef34888dc0de6444304b0e85ed214de837b000de
olia12 <686371olia@mail.ru>
2021-05-14 16:35:42 +0300

    create: swiper keyboard control

--- js/main.js
+++ js/main.js
@@ -7,4 +7,7 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
+  keyboard: {
+    enabled: true,
+  }
 });
