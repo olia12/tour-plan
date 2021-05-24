f65decc2ef09dee2c7af7ea2724db4f5e0c0c47b
olia12 <686371olia@mail.ru>
2021-05-25 00:55:09 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -21,3 +21,10 @@ var reviewsSwiper = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   }
 });
+
+var menuButton = document.querySelector(".menu-button")
+menuButton.addEventListener('click', function(){
+  console.log('Клик по кнопке меню')
+  document.querySelector(".navbar-bottom")
+  .classList.toggle('navbar-bottom--visible')
+});
